605264aedbc9981a27ca029d09efa44af8aea9a9
pvillor <pvillor@gmail.com>
2024-04-07 04:10:11 -0300

    feat: connect to api

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -6,17 +6,48 @@ import { Button } from "@/components/button";
 import { Link, router } from "expo-router";
 import { StatusBar } from "react-native";
 import { useState } from "react";
+import { api } from "@/server/api";
+import axios from "axios";
 
 export default function Home() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  function handleRegister() {
-    if (!name.trim() || !email.trim()) {
-      return Alert.alert("Inscrição", "Preencha todos os campos!");
+  const EVENT_ID = "179438c4-bdf8-4446-919a-9e915880a39d";
+
+  async function handleRegister() {
+    try {
+      if (!name.trim() || !email.trim()) {
+        return Alert.alert("Inscrição", "Preencha todos os campos!");
+      }
+      setIsLoading(true);
+
+      const registerResponse = await api.post(`/events/${EVENT_ID}/attendees`, {
+        name,
+        email,
+      });
+
+      if (registerResponse.data.attendeeId) {
+        Alert.alert("Inscrição", "Inscrição realizada com sucesso!", [
+          { text: "OK", onPress: () => router.push("/ticket") },
+        ]);
+      }
+    } catch (error) {
+      console.log(error);
+
+      if (axios.isAxiosError(error)) {
+        if (
+          String(error.response?.data.message).includes("already registered")
+        ) {
+          Alert.alert("Inscrição", "Este e-mail já está cadastrado!");
+        }
+      }
+
+      Alert.alert("Inscrição", "Não foi possível fazer a inscrição");
+    } finally {
+      setIsLoading(false);
     }
-
-    router.push("/ticket");
   }
 
   return (
@@ -51,7 +82,11 @@ export default function Home() {
           />
         </Input>
 
-        <Button title="Realizar inscrição" onPress={handleRegister} />
+        <Button
+          title="Realizar inscrição"
+          onPress={handleRegister}
+          isLoading={isLoading}
+        />
 
         <Link
           href="/"
